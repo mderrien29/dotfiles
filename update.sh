ee138183de79d9811e5640f82046a6d77567411d
Martial Derrien <derrienmartial@gmail.com>
2019-09-01 21:19:20 +0200

    :fire: (update) removing useless lines

--- update.sh
+++ update.sh
@@ -9,10 +9,7 @@ ln -sf $PWD/.zshrc ~/.zshrc
 ln -sf $PWD/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf $PWD/.config/nvim/local_init.vim  ~/.config/nvim/local_init.vim 
 ln -sf $PWD/.config/nvim/local_bundles.vim ~/.config/nvim/local_bundles.vim
-mkdir -p ~/.config/nvim/autoload ~/.config/nvim/colors ~/.config/nvim/autoload/lightline/
 ln -sf $PWD/.config/nvim/autoload/plug.vim  ~/.config/nvim/autoload/plug.vim
-ln -sf $PWD/.config/nvim/autoload/lightline/colorscheme  ~/.config/nvim/autoload/lightline/colorscheme
-ln -sf $PWD/.config/nvim/colors/shades_of_purple.vim ~/.config/nvim/colors/shades_of_purple.vim
 
 # alacritty 
 ln -sf $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
