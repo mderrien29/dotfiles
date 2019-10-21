673d54b663631038b9be6099737c669f9cb11059
martial <martial.derrien@niji.fr>
2019-10-21 15:38:56 +0200

    feat(i3status): new version

--- i3/i3-gaps.sh
+++ i3/i3-gaps.sh
@@ -1,7 +1,6 @@
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-xrm-dev
 
 
-cd tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 git checkout gaps && git pull
