bb2c21cc5549d31749f0c9f715d9a3c028a0848c
martial <derrienmartial@gmail.com>
2020-02-25 09:32:00 +0100

    :sparkles: (zsh) plugin added to install

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 # zsh
 ln -sf $PWD/.zshrc ~/.zshrc 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/softmoth/zsh-vim-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vim-mode
 
 # nvim
 ln -sf $PWD/.config/* ~/.config/
